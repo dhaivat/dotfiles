9e0f3e1713ffffb73769999a420e068109de20c2
Dhaivat Pandit <dhaivat.pandit@sojern.com>
2019-04-17 21:05:24 -0700

    changes to tmux, exports, spacemacs

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,7 +3,6 @@
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 export WS="$HOME/workspace"
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
-export GOPATH="$WS/go"
-export GOHOME="$GOPATH/src/github.com/ceocoder"
+export GOFLAGS=-mod=vendor
 export PATH=$AWS_IAM_HOME/bin:$EC2_LOC/bin:$PATH:$GOROOT/bin:$GOPATH/bin
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
