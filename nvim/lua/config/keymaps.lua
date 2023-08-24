5101a1c15a5641c37a39e317fd2a759031334c57
Dhaivat Pandit <dhaivatpandit@gmail.com>
2023-08-23 21:22:28 -0700

    reassign ? to show diagnostics for line under cursor
    since I don't use it for search

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- since I only use / for searches reassigning ? to show diagnostic for word/line under cursor
+map("n", "?", vim.diagnostic.open_float, { desc = "Show Line Diagnostic", remap = true })
